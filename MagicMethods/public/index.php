25afdc1cbf7d104b62aaf2df6d78118dee108a6e
Santiago Mena <santiagomenape@gmail.com>
2022-08-10 17:57:48 -0300

    Métodos Mágicos: __set & __get

--- MagicMethods/public/index.php
+++ MagicMethods/public/index.php
@@ -5,9 +5,12 @@ use Source\User;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$user = new User([
+$user = new User();
+$user->fill([
     'first_name' => 'Jhon',
-    'last_name' => 'Doe'
+    'last_name' => 'Doe',
+    'nickname' => 'jhondoe'
 ]);
-
+echo "Nickname: {$user->nickname}\n";
 echo "Bienvenido {$user->first_name} {$user->last_name}\n";
+print_r($user->getAttributes());
--- MagicMethods/src/User.php
+++ MagicMethods/src/User.php
@@ -3,9 +3,42 @@ namespace Source;
 
 class User
 {
+    protected array $attributes = [];
+
     public function __construct(array $attributes = []) {
-        foreach ($attributes as $key => $value) {
-            $this->$key = $value;
+        $this->fill($attributes);
+    }
+
+    public function getAttribute(string $name)
+    {
+        if(array_key_exists($name, $this->attributes)) {
+            return $this->attributes[$name];
         }
+    }
+    public function __get($name)
+    {
+        return $this->getAttribute($name);
+    }
+
+    public function setAttribute($name, $value)
+    {
+        $this->attributes[$name] = $value;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->setAttribute($name, $value);
+    }
+
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    public function fill(array $attributes): self
+    {
+        $this->attributes = $attributes;
+
+        return $this;
     }
 }
