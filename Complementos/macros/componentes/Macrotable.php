1a4cffe85eb41ef0440e8e7c22816f0478f6835e
Santiago Mena <santiagomenape@gmail.com>
2023-03-18 17:37:21 -0300

    Macros con traits

---
+++ Complementos/macros/componentes/Macrotable.php
@@ -0,0 +1,30 @@
+<?php
+namespace Php\Componentes;
+
+use Closure;
+
+trait MacroTable
+{
+    private static $macros = [];
+
+    public static function hasMacro($method)
+    {
+        return isset(static::$macros[$method]);
+    }
+
+    // Macro
+    public static function macro($method, Closure $macro)
+    {
+        static::$macros[$method] = $macro;
+    }
+
+    public function __call($method, array $arguments)
+    {
+        if(static::hasMacro($method)) {
+            return call_user_func_array(static::$macros[$method], $arguments);
+        }
+
+        throw new \BadMethodCallException("The method {$method} does not exist");
+        
+    }
+}
