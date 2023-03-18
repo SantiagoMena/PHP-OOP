0b2257aec84c91075d207af4bb161627a3dcd2be
Santiago Mena <santiagomenape@gmail.com>
2023-03-18 09:09:00 -0300

    Scooter params Fabrica Vehiculo interfaz

--- 23Patrones/src/AbstractFactory/FabricaVehiculoInterfaz.php
+++ 23Patrones/src/AbstractFactory/FabricaVehiculoInterfaz.php
@@ -7,5 +7,5 @@ interface FabricaVehiculoInterfaz
 {
     public function crearAutomovil(string $marca, string $color, int $potencia, float $espacio): AbstractAutomovil;
 
-    public function crearScooter(string $marca, string $color, int $potencia, float $espacio): AbstractScooter;
+    public function crearScooter(string $marca, string $color, int $potencia): AbstractScooter;
 }
