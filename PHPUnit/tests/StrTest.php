7129df6b34d7eb54a33a7447eaa11346a4cbd522
Santiago Mena <santiagomenape@gmail.com>
2023-03-18 18:23:19 -0300

    Creando tests para Str

--- PHPUnit/tests/StrTest.php
+++ PHPUnit/tests/StrTest.php
@@ -1,9 +1,26 @@
 <?php
+use Php\PhpUnit\Str;
 
 class StrTest extends PHPUnit\Framework\TestCase
 {
     public function test_test()
     {
-        exit('Test!');
+        $this->assertSame(
+            'Name', 
+            Str::studly('name'), 
+            "Error, no convierte la cadena deseada name esperado Name"
+        );
+
+        $this->assertSame(
+            'FullName', 
+            Str::studly('full_name'), 
+            "Error, no convierte la cadena deseada full_name esperado FullName"
+        );
+
+        $this->assertSame(
+            'BirthDate', 
+            Str::studly('birth_date'), 
+            "Error, no convierte la cadena deseada birth_date esperado BirthDate"
+        );
     }
 }
