283598f2e44bddb4befc398159842ef0e37fcf69
Santiago Mena <santiagomenape@gmail.com>
2023-03-18 19:05:07 -0300

    Refactor

--- PHPUnit/src/Model.php
+++ PHPUnit/src/Model.php
@@ -1,6 +1,7 @@
 <?php
 namespace Php\PhpUnit;
-abstract class Model
+use ArrayAccess;
+abstract class Model implements ArrayAccess
 {
     public string $first_name;
     public string $last_name;
@@ -15,5 +16,24 @@ abstract class Model
 
         if(isset($arguments['age']))
             $this->age = $arguments['age'];
+    }
+    public function offsetExists($offset): bool
+    {
+        return isset($this->$offset);
+    }
+
+    public function offsetGet($offset): mixed
+    {
+        return $this->$offset;
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->$offset = $value;
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->$offset);
     }
 }
--- PHPUnit/tests/ModelArrayAccessTest.php
+++ PHPUnit/tests/ModelArrayAccessTest.php
@@ -52,24 +52,7 @@ class ModelArrayAccessTest  extends PHPUnit\Framework\TestCase
     }
 }
 
-class UserTest extends Model implements ArrayAccess{
-    public function offsetExists($offset): bool
-    {
-        return isset($this->$offset);
-    }
-
-    public function offsetGet($offset): mixed
-    {
-        return $this->$offset;
-    }
-
-    public function offsetSet($offset, $value): void
-    {
-        $this->$offset = $value;
-    }
-
-    public function offsetUnset($offset)
-    {
-        unset($this->$offset);
-    }
+class UserTest extends Model
+{
+    
 }
