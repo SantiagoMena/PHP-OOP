4592b30ab0d848ece198b122d8e7ac0274cc41a4
Santiago Mena <santiagomenape@gmail.com>
2023-03-18 09:14:42 -0300

    Clase Catalogo

---
+++ 23Patrones/src/AbstractFactory/Catalogo.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Php\Oop\AbstractFactory;
+
+class Catalogo
+{
+    public function __construct(protected FabricaVehiculoInterfaz $fabricaVehiculo) { }
+
+    public function crearScooter(string $marca, string $color, int $potencia): AbstractScooter
+    {
+        return $this->fabricaVehiculo->crearScooter(marca: $marca, color: $color, potencia: $potencia);
+    }
+
+    public function crearAutomovil(string $marca, string $color, int $potencia, int $espacio): AbstractAutomovil
+    {
+        return $this->fabricaVehiculo->crearAutomovil(marca: $marca, color: $color, potencia: $potencia, espacio: $espacio);
+    }
+}
